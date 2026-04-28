ec522276f3f7f8130489eebf51c871cadc71cf09
makarov-hub465 <makarovsw@gmail.com>
2026-04-28 16:48:22 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,17 @@
 import os
 from dotenv import load_dotenv
 
-# Загружаем переменные из файла .env
+# Загружаем переменные из файла .env (если он есть локально)
 load_dotenv()
 
-# Получаем данные
-BOT_TOKEN = os.getenv('TG_TOKEN')
-ADMIN_ID = int(os.getenv('ADMIN_ID'))
-SHEET_ID = os.getenv('SHEET_ID')
-DONATE_LINK = os.getenv('DONATE_LINK')
+# Получаем данные из переменных окружения Render
+# Если переменной нет, используем запасной вариант (для локального теста)
+TG_TOKEN = os.getenv('TG_TOKEN') or '8680234949:AAHMsx6vSeKvWyiiWJjDIC77PtJ0o7H_Ctw'
+ADMIN_ID = int(os.getenv('ADMIN_ID') or '1161773989')
+SHEET_ID = os.getenv('SHEET_ID') or '1R4Y0AKwwmaXd_MXoyHmd7gycrfPiY6T9X_MOEW5mzPs'
+DONATE_LINK = os.getenv('DONATE_LINK') or 'https://pay.cloudtips.ru/p/0a1a32b5'
 CREDS_FILE = 'credentials.json'
 
-# Проверка на случай, если что-то забыли добавить в .env
-if not BOT_TOKEN or not SHEET_ID:
-    raise ValueError("❌ Ошибка: Проверь файл .env. Не хватает TG_TOKEN или SHEET_ID")
+# Проверка (опционально)
+if not TG_TOKEN:
+    raise ValueError("❌ Ошибка: Не задан TG_TOKEN")
